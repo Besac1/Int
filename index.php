3ff87003f0bb818b4689d92051c5e4fe9413c332
CHOLLET Axel <cholletax@orange.fr>
2015-07-23 14:38:27 +0200

    Ajout de l'index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang = 'fr'>
+
+<head>
+
+    <title>Int</title>
+    <link rel = "stylesheet" type = "text/css" href = "CSS/style.css">
+    <meta charset = 'UTF-8'/>
+
+</head>
+
+
+<body>
+
+    <section id = 'mainpage'>
+
+        <h1>Int</h1>
+        <p id = 'description'>Conversion de phrases en équivalent de valeurs dans l'alphabet</p>
+            
+        <div id = "partieUn">
+            
+            <div id = 'textarea'>
+                
+                <textarea id = 'string' placeholder = 'Veuillez saisir du texte '></textarea>
+                
+            </div>
+                    
+            <div id = 'display'>
+                        
+                <p>Additions des lettres : <span id = 'number'>0</span></p>
+                <p>Nombres de charactères <span id = 'nbCharacters'>0</span></p>
+                    
+            </div>  
+
+        </div>
+
+    </section>
+
+    <script type = "text/javascript" src = 'JS/script.js'></script>
+
+</body>
+
+
+</html>
